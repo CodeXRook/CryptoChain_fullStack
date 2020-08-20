a7452ce06008b1baf1c1843834bb4b90b3ae945f
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-19 20:08:38 -0400

    constructor complete need new const

--- block.js
+++ block.js
@@ -1,6 +1,11 @@
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
+        this.lastHash =  lastHash;
+        this.hash = hash;
+        this.data =data;
 
     }
 }
+
+const block1 = new Block();
