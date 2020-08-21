00f8192c1761ac7789b8c4bb11b680ca1d33493e
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 11:09:13 -0400

    config js will hold all hard coded dummy data values

--- block.text.js
+++ block.text.js
@@ -1,15 +1,16 @@
-describe('Block', ()=> {
+const Block = require('./block');
+
+describe('Block', () => {
     const timestamp = ' a-date';
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
-    const block = new Block ({timestamp, lastHash, hash, data});
+    const block = new Block ({ timestamp, lastHash, hash, data });
 
     it('has a timestamp, lastHash hash, and data property', () => {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
-
     });
 });
