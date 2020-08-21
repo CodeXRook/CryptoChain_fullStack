3d1ab0aa49d59040804a768112179bce3bb8fe75
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 18:26:30 -0400

    test passed, goddam the kid getting nice doeeeeeeeee

--- block.js
+++ block.js
@@ -12,6 +12,14 @@ class Block {
         return new this (GENESIS_DATA);
         
     }
+
+    static mineBlock({ lastBlock, data}) {
+        return new this({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data
+        });
+    }
 }
 
 module.exports = Block;
