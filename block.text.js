3b07e0e46e41f15627b979d543b52981d95e065a
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 18:05:31 -0400

    test passed

--- block.text.js
+++ block.text.js
@@ -18,6 +18,8 @@ describe('Block', () => {
     describe('genesis()', () => {
         const genesisBlock = Block.genesis(); 
 
+        console.log('genesisBlock', genesisBlock);
+
         it('returns a Block instance', () => {
             expect(genesisBlock instanceof Block).toBe(true);
         });
