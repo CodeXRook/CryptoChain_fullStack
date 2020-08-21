4a3df35898fdbdb2824ce1512ae070a09ab20995
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 10:47:28 -0400

    last block include all const

--- block.text.js
+++ block.text.js
@@ -1,7 +1,13 @@
 describe('Block', ()=> {
     const timestamp = ' a-date';
     const lastHash = 'foo-hash';
-    const hash;
-    const block;
+    const hash = 'bar-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block ({
+        timestamp: timestamp,
+        lastHash: lastHash,
+        hash: hash,
+        data: data
+    });
 
 });
