4de793619ec3d1cb19e1b235edd9fe2e1985f310
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 11:18:20 -0400

    text set up for genesis data

--- block.text.js
+++ block.text.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
 
 describe('Block', () => {
     const timestamp = ' a-date';
@@ -16,5 +17,14 @@ describe('Block', () => {
 
     describe('genesis()', () => {
         const genesisBlock = Block.genesis(); 
-    })
+
+        it('returns a Block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        })
+
+    });
 });
