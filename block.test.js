310e66a8cd49883a0d315e12f88b495b67c8a8b2
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 18:19:02 -0400

    add new test for timestamp

--- block.test.js
+++ block.test.js
@@ -31,14 +31,22 @@ describe('Block', () => {
     describe('mineBlock()', () => {
         const lastBlock = Block.genesis();
         const data = "mined data";
-        const mineBlock = Block.mineBlock({ lastBlock, data});
+        const minedBlock = Block.mineBlock({ lastBlock, data});
 
         it('returns a Block instance', () => {
            expect(minedBlock instanceof Block).toBe(true);
         });
 
-        it('sets the `lastHash` to be the `hash` of the lastBlock', () => ) {
+        it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+          expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        }); 
+        
+        it('sets the `data`', () => {
+            expect(minedBlock.data).toEqual(data);
+        });
+
+        it('sets a `timestamp`', () =>{
             
-        }
+        })
     });
 });
