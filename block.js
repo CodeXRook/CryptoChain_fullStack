a3888108b1f7a1766488011efdc62bdd46bf3083
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 10:40:20 -0400

    text code with module exports

--- block.js
+++ block.js
@@ -1,17 +1,11 @@
 class Block {
-    constructor({timestamp, lastHash, hash, data}) {
-        this.timestamp = timestamp;
-        this.lastHash =  lastHash;
-        this.hash = hash;
-        this.data =data;
+    // // constructor({timestamp, lastHash, hash, data}) {
+    // //     this.timestamp = timestamp;
+    // //     this.lastHash =  lastHash;
+    // //     this.hash = hash;
+    // //     this.data =data;
 
-    }
+    // }
 }
 
-const block1 = new Block({
-    timestamp: 'o1/01/01', 
-    lastHash: 'foo-lastHash', 
-    hash: 'foo-hash',
-    data: 'foo-data'});
-
-console.log('block1', block1);
+module.exports = Block;
