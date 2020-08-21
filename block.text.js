eb66bcbdc2d669c0bc1a503898764387cff51139
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-21 10:49:13 -0400

    can store and use all const as a key in new block

--- block.text.js
+++ block.text.js
@@ -3,11 +3,5 @@ describe('Block', ()=> {
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
-    const block = new Block ({
-        timestamp: timestamp,
-        lastHash: lastHash,
-        hash: hash,
-        data: data
-    });
-
+    const block = new Block ({timestamp, lastHash, hash, data});
 });
