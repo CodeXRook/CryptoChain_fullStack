74884566e564cd4ffdee315eb361b89124c2911a
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-22 06:42:51 -0400

    new test produces should produce same input argument in any order

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -1,9 +1,11 @@
 const crytpoHash = require('./crypto-hash');
 
 describe('cryptoHash()', ()=> {
-    "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
-
     it('generates a SHA-256 hashed output',() => {
+        expect(cryptoHash('foo')).toEqual('2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae');
+    });
+
+    it('produces the same hash with the same input arguments in any order',() => {
 
     });
 });
