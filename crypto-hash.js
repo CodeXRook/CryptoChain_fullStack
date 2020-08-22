14b4bf30b53a902a8bd36f153660b96e0ff323a3
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-22 06:58:55 -0400

    test completd passed all 9 test

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,4 +1,12 @@
-const crytpoHash = () => {};
+const crypto = require('crypto');
+
+const cryptoHash = (...inputs) => {
+ const hash = crypto.createHash('sha256');
+
+ hash.update(inputs.sort().join(' '));
+
+ return hash.digest('hex');
+};
 
 
 
