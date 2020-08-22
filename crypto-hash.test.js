4f273de8c86707a2fb004983fe6ab25c72f6c13f
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-22 06:38:48 -0400

    generating a foo hash, test should show same output in lowerCase

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -1,1 +1,9 @@
 const crytpoHash = require('./crypto-hash');
+
+describe('cryptoHash()', ()=> {
+    "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+
+    it('generates a SHA-256 hashed output',() => {
+
+    });
+});
