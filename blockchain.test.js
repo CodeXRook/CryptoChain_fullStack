dd9608d959b998957ca857a73796b8984da04a8d
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-23 19:09:43 -0400

    added chain and genesis for Array in test

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,5 +2,13 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('blockchain', () => {
-    
-})
+    const blockchain = new Blockchain();    
+
+    it('contains a `chain` Array instance', () => {
+        expect(blockchain.chain instanceof Array).toBe(true);
+    });
+
+    it('starts with the genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis());
+    });
+});
