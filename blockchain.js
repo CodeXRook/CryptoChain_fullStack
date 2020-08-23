bfa2dc4dcb0803397c8c3f38f6eb8d4e703dc620
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-23 19:20:56 -0400

    added all blocks

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,18 @@
-class Blockchain {
+const Block = require('.//block');
 
+class Blockchain {
+constructor() {
+    this.chain = [ Block.genesis()];
+}
+
+addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+        lastBlock: this.chain[this.chain.length-1],
+        data
+    });
+
+    this.chain.push(newBlock);
+ }
 
 }
 
