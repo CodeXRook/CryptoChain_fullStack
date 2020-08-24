d57b991a8424a877b777d3dd5e5222a7e418ab29
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-23 20:26:08 -0400

    change const to let to get new instance of the block, also added beforeEach to the it function to change each block to genesis state

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,13 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-    const blockchain = new Blockchain();    
+    let blockchain;
+    
+    beforeEach(() => {
+        blockchain = new Blockchain();
+    });
+    
+    
 
     it('contains a `chain` Array instance', () => {
         expect(blockchain.chain instanceof Array).toBe(true);
@@ -22,12 +28,25 @@ describe('Blockchain', () => {
 
     describe('isValidChain()', () => {
         describe('when the chain does not start with the genesis block', () => {
-            it('returns false', () => {});
+            it('returns false', () => {
+                blockchain.chain[0] = { data: 'fake-genesis' };
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            });
         });
 
         describe('when the chain starts with the genesis block and has multiple blocks', () => {
             describe('and a lastHash reference has changed', () => {
-                it('returns a false', () => {});
+                it('returns a false', () => {
+                    blockchain.addBlock({ data: 'Bears' });
+                    blockchain.addBlock({ data: 'Beet' });
+                    blockchain.addBlock({ data: 'Battlestar Galactica' });
+
+                    blockchain.chain[2].lastHash = 'broken-lastHash';
+
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+                });
             });
 
             describe('and the chain contains a block with an invalid field', () => {
@@ -36,7 +55,7 @@ describe('Blockchain', () => {
 
             describe('and the chain does not contain any invalid blocks',  () => {
                 it('returns true', () =>{
-                    
+
                 });
             })
         });
