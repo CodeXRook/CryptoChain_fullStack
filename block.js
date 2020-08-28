7714d74ac07819fda0d36434790d8f221ace9bb6
CodeXrook <dtkprpt@CodeXRook.local>
2020-08-27 22:43:14 -0400

    added nonce and difficulty to the block so it can be identified in test

--- block.js
+++ block.js
@@ -2,11 +2,13 @@ const {GENESIS_DATA} = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
-    constructor({timestamp, lastHash, hash, data}) {
+    constructor({timestamp, lastHash, hash, data nonce, difficulty}) {
         this.timestamp = timestamp;
         this.lastHash =  lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
+        this.difficulty = difficulty;
     }
 
     static genesis() {
