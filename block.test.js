8b231b603ba935ac80d7e0ee79a7a324b17e824c
CodeXrook <dtkprpt@CodeXRook.local>
2020-09-02 22:50:09 -0400

    installed npm hex to binary

--- block.test.js
+++ block.test.js
@@ -91,5 +91,11 @@ describe('Block', () => {
                 originalBlock: block, timestamp: block.timestamp + MINE_RATE + 100
             })).toEqual(block.difficulty-1);
         });
+
+        it('has a lower limit of 1', () => {
+            block.difficulty = -1;
+
+            expect(Block.adjustDifficulty({ originalBlock: block })).toEqual
+        });
     });
 });
--- crypto-hash.js
+++ crypto-hash.js
@@ -1,11 +1,12 @@
 const crypto = require('crypto');
+const hexToBinary = require('hex-to-binary');
 
 const cryptoHash = (...inputs) => {
  const hash = crypto.createHash('sha256');
 
  hash.update(inputs.sort().join(' '));
 
- return hash.digest('hex');
+ return hexToBinary(hash.digest('hex'));
 };
 
 
