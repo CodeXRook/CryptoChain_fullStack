ad7d6b4004713c3caf4db8b24917a5478ac42965
CodeXrook <lacekitfly@gmail.com>
2020-09-03 18:19:24 -0400

    test works

--- block.js
+++ block.js
@@ -1,5 +1,7 @@
+const hexToBinary = require('./crypto-hash');
 const {GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
+
 
 class Block {
     constructor({timestamp, lastHash, hash, data, nonce, difficulty }) {
@@ -27,7 +29,7 @@ class Block {
          timestamp = Date.now();
          difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
          hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while (hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({ timestamp, lastHash, data, difficulty, nonce, hash });
     }
