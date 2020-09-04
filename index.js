9a663ae54ec210499d4b81f4cb3e96b9af4e2e78
CodeXrook <lacekitfly@gmail.com>
2020-09-04 09:43:10 -0400

    added new port but can't see get in browser from local host,???

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
+
+app.get('.api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
