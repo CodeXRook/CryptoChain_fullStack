27d49c9f2e60885a896543308807ffa48502bbbb
CodeXrook <lacekitfly@gmail.com>
2020-09-05 20:10:39 -0400

    implenting pubsub, smfh,

--- pubsub.js
+++ pubsub.js
@@ -1,27 +1,48 @@
-const redis = require('redis');
+const PubNub = require('pubnub');
 
-const CHANNELS = {
-    TEST: 'TEST'
+const credentials = {
+ publishKey: 'pub-c-bef8b219-5c58-4fa9-b535-4f38a057919e',
+ subscribeKey: 'sub-c-4048e32e-efd4-11ea-a728-4ec3aefbf636',
+ secretKey: 'sec-c-YjlhMzM3OGItNGE5NS00NWMxLWFhYjItOWJlMjcwNjAxYzVj'
 };
 
-class PubSub{
-    constructor(){
-        this.publisher = redis.createClient();
-        this.subscriber = redis.createClient();
 
-        this.subscriber.subscribe(CHANNELS.TEST);
-
-        this.subscriber.on(
-            'message', 
-            (channel, message) => this.handleMessage(channel, message)
-        );
-    }
-
-    handleMessage(channel, message) {
-        console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
+class PubSub {
+    constructor() {
+        this.pubnub = new PubNub(creadentials);
     }
 }
 
-const testPubSub = new PubSub();
 
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+
+
+
+
+
+// const redis = require('redis');
+
+// const CHANNELS = {
+//     TEST: 'TEST'
+// };
+
+// class PubSub{
+//     constructor(){
+//         this.publisher = redis.createClient();
+//         this.subscriber = redis.createClient();
+
+//         this.subscriber.subscribe(CHANNELS.TEST);
+
+//         this.subscriber.on(
+//             'message', 
+//             (channel, message) => this.handleMessage(channel, message)
+//         );
+//     }
+
+//     handleMessage(channel, message) {
+//         console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
+//     }
+// }
+
+// const testPubSub = new PubSub();
+
+// setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
