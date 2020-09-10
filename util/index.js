a2cb59704196366deafcc25fcbfc63169e5e94c1
CodeXrook <lacekitfly@gmail.com>
2020-09-10 01:26:52 -0400

    added key from public

--- util/index.js
+++ util/index.js
@@ -2,5 +2,8 @@ const EC = require('elliptic').ec;
 
 const ec = new EC('secp256k1');
 
-const verifySignature = ({ publicKey, data, signature }) => {};
-module.exports = { ec };
+const verifySignature = ({ publicKey, data, signature }) => {
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+};
+
+module.exports = { ec, verifySignature };
