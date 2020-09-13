e0947b1d4f5a6c99b92ac795d9b93cbd4046c705
CodeXrook <lacekitfly@gmail.com>
2020-09-13 15:40:31 -0400

    add try catch to code

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Transaction = require('./wallet/transaction');
+const Wallet = require('./wallet');
 // const { ReplyError } = require('redis');
-
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -26,6 +30,21 @@ app.post('/api/mine', (req, res) => {
     pubsub.broadcastChain();
 
     res.redirect('/api/blocks');
+});
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+
+    try {
+        const transaction = wallet.createTransaction({ recipient, amount });
+    } catch(error) {
+        res.json({ type: 'error', message: error.message });
+    }
+
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool', transactionPool);
+
+    res.json({ transaction });
 });
 
 const syncChains = () => {
--- wallet/transaction-pool-test.js
+++ wallet/transaction-pool-test.js
@@ -1,1 +1,25 @@
 const TransactionPool = require('./transaction-pool');
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('TransactionPool', () => {
+    let transactionPool, transaction;
+
+    beforeEach(() => {
+        transactionPool = new TransactionPool();
+        transaction = new Transaction({
+            senderWallet: new Wallet(),
+            recipient: 'fake-recipient',
+            amount: 50
+        });
+    });
+
+    describe('setTransaction()', () => {
+        it('adds a transaction', () => {
+            transactionPool.setTransaction(Transaction);
+
+            expect(transactionPool.transactionMap[transaction.id])
+            .toBe(Transaction);
+        });
+    });
+});
