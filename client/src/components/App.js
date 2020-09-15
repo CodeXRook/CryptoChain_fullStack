7fd1ebc62cfc8f552767ef9c7ea78514fcb09ceb
CodeXrook <lacekitfly@gmail.com>
2020-09-14 22:16:38 -0400

    set up then method

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
+import { response } from 'express';
 import React, { Component } from 'react';
 
 class App extends Component {
     state = { walletInfo: {address: 'fooxv6', balance: 9999 } };
 
     componentDidMount(){
-        fetch('http://localhost:3000/api/wallet-info');
+        fetch('http://localhost:3000/api/wallet-info')
+        .then(response => console.log('response', response));
     }
 
   render() {
