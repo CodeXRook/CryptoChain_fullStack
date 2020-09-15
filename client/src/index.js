6f5a916de91e1dd75c3a55f714286d41eac6a1fc
CodeXrook <lacekitfly@gmail.com>
2020-09-14 21:39:02 -0400

    testing in place, set my local env in package json

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-  <div>Cryptochain in React!</div>, 
+  <div>Cryptochain in React!: Third Live Update</div>,
   document.getElementById('root')
   );
