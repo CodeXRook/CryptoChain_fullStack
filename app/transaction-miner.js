3efa347f8503a2314eeb38071e7993256d58edd0
CodeXrook <lacekitfly@gmail.com>
2020-09-16 15:54:07 -0400

    need to style app now, work on bootstrap

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,11 +1,11 @@
 const Transaction = require('../wallet/transaction');
 
 class TransactionMiner {
-    constructor({ blockchain, transactionPool, wallet, pusub }) {
+    constructor({ blockchain, transactionPool, wallet, pubsub }) {
         this.blockchain = blockchain;
         this.transactionPool = transactionPool;
         this.wallet = wallet;
-        this.pusub = pusub;
+        this.pubsub = pubsub;
     }
 
     mineTransactions() {
