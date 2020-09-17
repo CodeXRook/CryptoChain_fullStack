b264c64fc91eb0b10326a06e3ad7711d15661a46
CodeXrook <lacekitfly@gmail.com>
2020-09-16 21:54:28 -0400

    show more, show less data, added bs button class

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
 
 class Block extends Component {
-    render() {
+    state = { displayTransaction: false };
 
+    toggleTransaction = () => {
+    this.setState({displayTransaction: !this.state.displayTransaction});
+    }
+
+    get displayTransaction() {
+    const { data } = this.props.block;
+
+    const stringifiedData = JSON.stringify(data);
+
+    const dataDisplay = stringifiedData.length > 35 ? 
+      `${stringifiedData.substring(0, 35)}...` :
+        stringifiedData;
+
+    if ( this.state.displayTransaction) {
+        return (
+            <div>
+                {JSON.stringify(data)}
+                <br />
+                <Button 
+                bsStyle="danger"
+                bsSize="small"
+                onClick={this.toggleTransaction}
+                >
+                Show Less
+                </Button>
+            </div>
+        )
+    }
+
+    return ( 
+    <div>
+         <div>Data: {dataDisplay}</div>
+        <Button
+          bsStyle="danger"
+          bsSize="small" 
+          onClick={this.toggleTransaction}
+        >
+          Show More
+        </Button>
+        </div>
+        );
+    }
+
+    render() {
+       const { timestamp, hash } = this.props.block;
+
+       const hashDisplay = `${hash.substring(0, 15)}...`;
+
+        return (
+            <div className='Block'>
+            <div>Hash: {hashDisplay}</div>
+            <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
+            {this.displayTransaction}
+            </div>
+        )
     }
 };
 
--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Block from './Block';
 
 
 class Blocks extends Component {
@@ -15,12 +16,12 @@ class Blocks extends Component {
 
         return (
             <div>
-                <h3>Bear Blocks</h3>
+                <h3>Blocks</h3>
                 {
                     this.state.blocks.map(block => {
                         return (
-                           <div key={block.hash} className='Block'>{block.hash}</div>
-                        )
+                            <Block key={block.hash} block={block} />
+                        );
                     })
                 }
             </div>
